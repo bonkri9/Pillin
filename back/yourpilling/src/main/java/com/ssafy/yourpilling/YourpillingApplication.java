0876b0066ce69bc8716cc1ba04b498b98a31f03f
강성범 <qkfka9045@gmail.com>
2024-01-20 17:25:13 +0900

    [initial] 백엔드 서버 개발 환경 셋팅

---
+++ back/yourpilling/src/main/java/com/ssafy/yourpilling/YourpillingApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.yourpilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourpillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourpillingApplication.class, args);
+	}
+
+}
