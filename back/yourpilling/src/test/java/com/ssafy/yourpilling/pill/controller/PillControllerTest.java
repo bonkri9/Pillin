b2fd3b102e4fde635260a41ae5744eea7586ae1c
강성범 <qkfka9045@gmail.com>
2024-01-23 20:10:16 +0900

    [test] 보유중인 영양제 등록 테스트 코드

--- back/yourpilling/src/test/java/com/ssafy/yourpilling/pill/controller/PillControllerTest.java
+++ back/yourpilling/src/test/java/com/ssafy/yourpilling/pill/controller/PillControllerTest.java
@@ -9,7 +9,6 @@ import com.ssafy.yourpilling.security.auth.PrincipalDetails;
 import com.ssafy.yourpilling.security.auth.entity.Member;
 import com.ssafy.yourpilling.security.auth.jwt.JwtManager;
 import com.ssafy.yourpilling.security.auth.repository.MemberRepository;
-import jakarta.persistence.EntityManager;
 import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,16 +20,15 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
 
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -54,42 +52,36 @@ class PillControllerTest {
     @Autowired
     private BCryptPasswordEncoder encoder;
 
-    @Autowired
-    private EntityManager em;
-
     @Test
     @DisplayName("보유중인 영양제 등록")
     public void register() throws Exception {
-//        // given
-//        Member member = defaultRegisterMember();
-//        Pill pill = defaultRegisterPill();
-//
-//
-//        List<Member> all = memberRepository.findAll();
-//        List<Pill> all1 = pillJpaRepository.findAll();
-//
-//        String accessToken = getAccessToken(member);
-//
-//        JSONObject body = new JSONObject();
-//        //body.put("pillId", pill.getPillId());
-//        body.put("startAt", LocalDate.now());
-//        body.put("takeYn", true);
-//        body.put("remains", 60);
-//        body.put("totalCount", 60);
-//        body.put("takeCount", 1);
-//
-//        Optional<Member> byId = memberRepository.findByMemberId(memberRepository.findByUsername(member.getUsername()).get().getMemberId());
-//        Member member1 = byId.get();
-//
-//        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
-//                .post("/api/v1/pill/inventory")
-//                .header("accessToken", accessToken)
-//                .content(body.toString())
-//                .contentType(MediaType.APPLICATION_JSON);
-//
-//        // when, then
-//        mockMvc.perform(request)
-//                .andExpect(status().isOk());
+        // given
+        Member member = defaultRegisterMember();
+        Pill pill = defaultRegisterPill();
+
+        String accessToken = getAccessToken(member);
+
+        JSONObject body = new JSONObject();
+        body.put("pillId", pill.getPillId());
+        body.put("startAt", LocalDate.now());
+        body.put("takeYn", true);
+        body.put("remains", 60);
+        body.put("totalCount", 60);
+        body.put("takeCount", 1);
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post("/api/v1/pill/inventory")
+                .header("accessToken", accessToken)
+                .content(body.toString())
+                .contentType(MediaType.APPLICATION_JSON);
+
+        // when
+        ResultActions perform = mockMvc.perform(request);
+
+
+        // then
+        perform.andExpect(status().isOk());
+        assertFalse(pillJpaRepository.findByName(pill.getName()).isEmpty());
     }
 
     private Pill defaultRegisterPill(){
@@ -111,7 +103,6 @@ class PillControllerTest {
 
         if(pillJpaRepository.findByName(pill.getName()).isEmpty()){
             pillJpaRepository.save(pill);
-            em.persist(pill);
         }
         return pill;
     }
@@ -131,7 +122,6 @@ class PillControllerTest {
 
         if(memberRepository.findByUsername(member.getUsername()).isEmpty()){
             memberRepository.save(member);
-            em.persist(member);
         }
         return member;
     }
