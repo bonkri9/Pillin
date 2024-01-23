76a8cbc5026ce9d6a913700a4ff50d7228c0cb17
구본민 <139411343+bonkri9@users.noreply.github.com>
2024-01-23 17:54:44 +0900

    [feat] 로그인 페이지

--- yourpilling_pjt/yourpilling/lib/screen/my_page_screen.dart
+++ yourpilling_pjt/yourpilling/lib/screen/my_page_screen.dart
@@ -1,11 +1,31 @@
-import 'package:flutter/cupertino.dart';
-import '../component/BaseContainer.dart';
+
+import 'package:flutter/material.dart';
+import 'package:yourpilling/component/login/login_main.dart';
+import 'package:yourpilling/component/login/sign_up_screen.dart';
+import '../component/login/login2_screen.dart';
 class MyPage extends StatelessWidget {
   const MyPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      body: Container(
+        // child: Text("하이"),
+        child: Row(
+          children: [
+            IconButton(onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context)=> LoginMain()));
+            },
+                icon: Icon(Icons.login, size: 50,),
+            ),
+            // IconButton(onPressed: () {
+            //   Navigator.push(context, MaterialPageRoute(builder: (context)=> SignupScreen()));
+            // },
+            //   icon: Icon(Icons.assignment_ind_outlined, size: 50,),
+            // ),
+          ],
+        ),
+      )
     );
   }
 }
