07e2062df4b1ab83ed04ab48fae2637bfb26e9ec
구본민 <139411343+bonkri9@users.noreply.github.com>
2024-01-26 17:51:21 +0900

    [feat] 상세 페이지 초안

---
+++ yourpilling_pjt/yourpilling/lib/component/pilldetail/search_pill_detail.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+
+import '../../const/colors.dart';
+
+class PillDetailScreen extends StatefulWidget {
+  const PillDetailScreen({super.key});
+
+  // int pillNo;
+  // String pillName;
+  // String pillImgUrl;
+  // int rank;
+  //
+  // PillDetailScreen(
+  //     {super.key,
+  //     required this.pillNo,
+  //     required this.pillName,
+  //     required this.pillImgUrl,
+  //     required this.rank});
+
+  @override
+  State<PillDetailScreen> createState() => _PillDetailScreenState();
+}
+
+class _PillDetailScreenState extends State<PillDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFFF5F4),
+      appBar: AppBar(
+        title: Text('영양제 상세 페이지'),
+        centerTitle: true,
+        backgroundColor: Color(0xFFFFF5F4),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(30),
+        child:  Center(
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            elevation: 4.0, //shadow
+            child: Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(60),
+                  child: Image(
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      fit: BoxFit.cover,
+                      image: AssetImage("assets/image/비타민B.jpeg")
+                    // imageUrl: widget.productImageUrl,
+                    // placeholder: (context, url){
+                    //   return const Center(
+                    //     child: CircularProgressIndicator(
+                    //       strokeWidth: 2,
+                    //     ),
+                    //   );
+                    // },
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: Text(
+                    // widget.pillName,
+                    // textScaleFactor: 1.5,
+                    // style: const TextStyle(
+                    //   fontWeight: FontWeight.bold,
+                    // ),
+                    '뉴트리코스트',
+                    style: TextStyle(fontWeight: FontWeight.w100),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical:10),
+                  child: Text(
+                    '비타민B',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Color(0xFFFFF5F4),
+        child: Row(
+          children: [
+            Expanded(
+              child: ElevatedButton(
+                onPressed: () {
+                  // '내 영양제 등록하기' 버튼 클릭 시 수행할 작업 추가
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.redAccent),
+                ),
+                child: Text(
+                  '내 영양제 등록하기',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(width: 16.0), // 간격 조절
+            Expanded(
+              child: ElevatedButton(
+                onPressed: () {
+                  // '구매하러 가기' 버튼 클릭 시 수행할 작업 추가
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.redAccent),
+                ),
+                child: Text(
+                  '구매하러 가기',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      // Center(
+      //   child: Column(
+      //     children: [
+      //       Padding(
+      //         padding:
+      //             const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      //         child: Column(
+      //           children: [
+      //             Image.asset(
+      //               "assets/image/비타민B.jpeg",
+      //               width: 350,
+      //               height: 350,
+      //             ),
+      //             Text('뉴트리코스트'),
+      //             Text('비타민B')
+      //           ],
+      //         ),
+      //       )
+      //     ],
+      //   ),
+      // ),
+    );
+  }
+}
--- yourpilling_pjt/yourpilling/lib/screen/inventory_screen.dart
+++ yourpilling_pjt/yourpilling/lib/screen/inventory_screen.dart
@@ -1,12 +1,306 @@
 import 'package:flutter/material.dart';
+import 'package:yourpilling/component/AngleContainer.dart';
+import 'package:yourpilling/component/app_bar_search.dart';
+import 'package:yourpilling/const/colors.dart';
+
+import 'package:yourpilling/component/pilldetail/search_pill_detail.dart';
+
+import '../component/BaseContainer.dart';
+import '../component/login/login_main.dart';
+import '../component/login/member_register.dart';
 
 class Inventory extends StatelessWidget {
   const Inventory({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("인벤토리"),
-    );
+    return Scaffold(
+        resizeToAvoidBottomInset : false,
+        appBar: MainAppBarSearch(barColor: Colors.white),
+        body: ListView(
+          children: [
+            AngleContainer(
+                width: 300,
+                height: 130,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back_ios)),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Text("성현",
+                              style: TextStyle(
+                                color: HOT_PINK,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w800,
+                              )),
+                          Text(" 님",
+                              style: TextStyle(
+                                color: BASIC_BLACK,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w800,
+                              )),
+                        ],
+                      ),
+                      Text(
+                        "오늘도 건강하세요!",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Container(
+                        height: 25,
+                        child: TextButton(
+                          style: ButtonStyle(
+                            minimumSize: MaterialStateProperty.all(Size.zero),
+                            padding: MaterialStateProperty.all(
+                                EdgeInsets.zero), // 버튼의 기본 패딩 없애기
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            "정보 수정",
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
+            // 설정 관리
+            AngleContainer(
+                width: 300,
+                height: 170,
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "설정 관리",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w800,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          print('버튼이 클릭되었습니다.');
+                        },
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.edit_notifications_outlined,
+                                  size: 21,
+                                ),
+                                SizedBox(width: 8.0),
+                                Text('알림 설정'),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.watch,
+                                  size: 19,
+                                ),
+                                SizedBox(width: 8.0),
+                                Text('건강데이터 및 기기 연동'),
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )),
+
+            // 고객 지원
+            AngleContainer(
+                width: 300,
+                height: 400,
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "고객 지원",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w800,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          print('버튼이 클릭되었습니다.');
+                        },
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.share_outlined,
+                                  size: 21,
+                                ),
+                                SizedBox(width: 8.0),
+                                Text('앱 공유하기'),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.comment_outlined,
+                                  size: 19,
+                                ),
+                                SizedBox(width: 8.0),
+                                Text('앱 스토어 리뷰 남기기'),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          print('버튼이 클릭되었습니다.');
+                        },
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.lock_outline_rounded,
+                                  size: 21,
+                                ),
+                                SizedBox(width: 8.0),
+                                Text('개인정보 처리 방침'),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.logout,
+                                  size: 19,
+                                ),
+                                SizedBox(width: 8.0),
+                                Text('로그아웃'),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            BaseContainer(
+                              width: 350,
+                              height: 35,
+                              child: TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              PillDetailScreen()));
+                                },
+                                child: Row(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text("영양제 상세페이지",
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                        )
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+
+                            BaseContainer(
+                              width: 350,
+                              height: 35,
+                              child: TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              LoginMain()));
+                                },
+                                child: Row(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text("로그인",
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                        )
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            BaseContainer(
+                              width: 350,
+                              height: 35,
+                              child: TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              MemberRegister()));
+                                },
+                                child: Row(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text("회원가입",
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                        )
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
+          ],
+        ));
   }
 }
