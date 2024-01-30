496a88feeb4d36aba465efdb7e71256225238f09
강성범 <qkfka9045@gmail.com>
2024-01-30 22:01:50 +0900

    [feat] 대표 건강고민 별 주간 랭킹 생성

--- back/yourpilling/src/main/java/com/ssafy/yourpilling/common/Nutrient.java
+++ back/yourpilling/src/main/java/com/ssafy/yourpilling/common/Nutrient.java
@@ -1,39 +1,42 @@
 package com.ssafy.yourpilling.common;
 
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
 public enum Nutrient {
-    VITAMINA("VitaminA", "비타민A"),
-    VITAMINB2("VitaminB2", "비타민B2"),
-    VITAMINB6("VitaminB6", "비타민B6"),
-    VITAMINB12("VitaminB12", "비타민B12"),
-    VITAMINC("VitaminC", "비타민C"),
-    VITAMIND("VitaminD", "비타민D"),
-    VITAMINE("VitaminE", "비타민E"),
-    VITAMINK("VitaminK", "비타민K"),
-    CALCIUM("Calcium", "칼슘"),
-    POTASSIUM("Potassium", "칼륨"),
-    MAGNESIUM("Magnesium", "마그네슘"),
-    MANGANESE("Manganese", "망간"),
-    SELENIUM("Selenium", "셀레늄"),
-    FOLIC_ACID("FolicAcid", "엽산"),
-    AMINO_ACID("AminoAcid", "아미노산"),
-    PANTOTHENIC_ACID("PantothenicAcid", "판토텐산"),
-    BIOTIN("Biotin", "비오틴"),
-    STEEL("Steel", "철"),
-    OMEGA3("Omega3", "오메가3");
+    VITAMINA("VitaminA", "비타민A", List.of(1, 8, 13)),
+    VITAMINB1("VitaminB1", "비타민B1", List.of(10)),
+    VITAMINB2("VitaminB2", "비타민B2", List.of(1, 2, 3, 5, 6)),
+    VITAMINB3("VitaminB3", "비타민B3", List.of(5)),
+    VITAMINB6("Vita minB6", "비타민B6", List.of(2, 5, 6, 12)),
+    VITAMINB12("VitaminB12", "비타민B12", List.of(2, 3, 4, 7, 9, 11, 12)),
+    VITAMINC("VitaminC", "비타민C", List.of(0, 2, 6, 9, 10, 13)),
+    VITAMIND("VitaminD", "비타민D", List.of(0, 6, 13)),
+    VITAMINE("VitaminE", "비타민E", List.of(13)),
+    VITAMINK("VitaminK", "비타민K", List.of(0, 9, 13)),
+    CALCIUM("Calcium", "칼슘", List.of(0, 1, 9, 10, 11)),
+    POTASSIUM("Potassium", "칼륨", List.of(0, 9, 10)),
+    MAGNESIUM("Magnesium", "마그네슘", List.of(0, 9)),
+    MANGANESE("Manganese", "망간", List.of(0, 1, 6, 9, 12, 13)),
+    SELENIUM("Selenium", "셀레늄", List.of(0, 3, 5, 9, 12, 13)),
+    FOLIC_ACID("FolicAcid", "엽산", List.of(1, 2, 5, 9, 11, 12)),
+    AMINO_ACID("AminoAcid", "아미노산", List.of(1, 2, 3, 5, 6, 9, 10, 11, 13)),
+    PANTOTHENIC_ACID("PantothenicAcid", "판토텐산", List.of(4, 5, 6, 10, 12)),
+    BIOTIN("Biotin", "비오틴", List.of(5, 6, 12, 13)),
+    STEEL("Steel", "철", List.of(2, 7, 11)),
+    OMEGA3("Omega3", "오메가3", List.of(2, 8, 9)),
+    CHROMIUM("Chromium", "크롬", List.of(9));
 
     private final String english;
     private final String korean;
 
-    Nutrient(String english, String korean) {
+    private final List<Integer> healthConcernIndex;
+
+    Nutrient(String english, String korean, List<Integer> healthConcernIndex) {
         this.english = english;
         this.korean = korean;
-    }
-
-    public String getEnglish() {
-        return english;
-    }
-
-    public String getKorean() {
-        return korean;
+        this.healthConcernIndex = healthConcernIndex;
     }
 }
--- back/yourpilling/src/main/java/com/ssafy/yourpilling/rank/model/service/impl/RankServiceImpl.java
+++ back/yourpilling/src/main/java/com/ssafy/yourpilling/rank/model/service/impl/RankServiceImpl.java
@@ -2,6 +2,7 @@ package com.ssafy.yourpilling.rank.model.service.impl;
 
 import com.ssafy.yourpilling.common.AgeGroup;
 import com.ssafy.yourpilling.common.Gender;
+import com.ssafy.yourpilling.common.HealthConcern;
 import com.ssafy.yourpilling.common.Nutrient;
 import com.ssafy.yourpilling.rank.model.dao.RankDao;
 import com.ssafy.yourpilling.rank.model.dao.entity.EachCountPerPill;
@@ -16,9 +17,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
+import java.util.*;
 
 import static java.time.LocalDateTime.*;
 
@@ -28,6 +27,7 @@ import static java.time.LocalDateTime.*;
 public class RankServiceImpl implements RankService {
 
     private final static int LIMIT = 5;
+    private final static int MAX_WEEK_YEAR = 52;
 
     private final RankDao rankDao;
     private final RankServiceMapper mapper;
@@ -35,7 +35,7 @@ public class RankServiceImpl implements RankService {
     @Scheduled(cron = "0 30 10 * * SUN")
     @Override
     public void generateWeeklyRank() {
-        int weeks = Calendar.getInstance().getWeekYear();
+        int weeks = (Calendar.getInstance().getWeekYear() + 1) % MAX_WEEK_YEAR;
         int year = LocalDate.now().getYear();
 
         List<Rank> infos = new ArrayList<>();
@@ -43,10 +43,8 @@ public class RankServiceImpl implements RankService {
         // 나이 및 성별
         ageAndGenderRank(weeks, year, infos);
 
-        // 영양소
-        nutrientRank(weeks, year, infos);
-
-        // 건강고민
+        // 영양소 및 건강고민
+        nutrientAndHealthConcernRank(weeks, year, infos);
 
         rankDao.registerAll(infos);
     }
@@ -64,11 +62,53 @@ public class RankServiceImpl implements RankService {
         }
     }
 
-    private void nutrientRank(int weeks, int year, List<Rank> infos){
+    private void nutrientAndHealthConcernRank(int weeks, int year, List<Rank> infos){
+        HealthConcern[] values = HealthConcern.values();
+        Map<Long, Long>[] pillAmountPerHealthConcern = new Map[values.length];
+        Arrays.fill(pillAmountPerHealthConcern, new HashMap<>());
+
         for (Nutrient n : Nutrient.values()) {
             List<EachCountPerPill> each = rankDao.rankNutrition(n.getEnglish());
 
-            limitSave(weeks, year, infos, each, getMidCategory(n.getEnglish()));
+            RankMidCategory midCategory = getMidCategory(n.getEnglish());
+
+            List<Integer> healthConcernsIndex = n.getHealthConcernIndex();
+
+            for(int i = 0; i<Math.min(LIMIT, each.size()); i++){
+                Long pillId = each.get(i).getPillId();
+                Long count = each.get(i).getPillCount();
+                
+                RankPill rankPill = rankDao.searchPillByPillId(pillId);
+
+                Rank save = mapper.mapToRank(weeks, year, i, rankPill, midCategory, now());
+                infos.add(save);
+
+                // 해당 영양소가 포함되는 건강 고민
+                for (Integer concernsIndex : healthConcernsIndex) {
+                    Map<Long, Long> pills = pillAmountPerHealthConcern[concernsIndex];
+                    pills.put(pillId, pills.getOrDefault(pillId, 0L) + count); // 영양소 정보에 맞는 영양제 정보 삽입
+                }
+            }
+        }
+
+        // 건강 고민
+        healthConcernRank(weeks, year, infos, pillAmountPerHealthConcern, values);
+    }
+
+    private void healthConcernRank(int weeks, int year, List<Rank> infos, Map<Long, Long>[] pillAmountPerHealthConcern, HealthConcern[] values) {
+        for(int k = 0; k< pillAmountPerHealthConcern.length; k++){
+            Map<Long, Long> h = pillAmountPerHealthConcern[k]; // key: 영양제 번호, value : 영양제 총 량
+
+            // 가장 많은 영양제를 가지고 있는 순으로 정렬
+            List<Long> desc = new ArrayList<>(h.keySet());
+            desc.sort((o1, o2) -> h.get(o2).compareTo(h.get(o1)));
+
+            for(int i=0; i<Math.min(LIMIT, desc.size()); i++){
+                RankPill rankPill = rankDao.searchPillByPillId(desc.get(i));
+
+                Rank save = mapper.mapToRank(weeks, year, i, rankPill, getMidCategory(values[k].getEnglish()), now());
+                infos.add(save);
+            }
         }
     }
 
@@ -76,11 +116,11 @@ public class RankServiceImpl implements RankService {
         return rankDao.searchMidCategoryByMidCategoryName(midCategoryName);
     }
 
-    private void limitSave(int weeks, int year, List<Rank> infos, List<EachCountPerPill> each, RankMidCategory midCategoryId) {
+    private void limitSave(int weeks, int year, List<Rank> infos, List<EachCountPerPill> each, RankMidCategory midCategory) {
         for(int i = 0; i<Math.min(LIMIT, each.size()); i++){
             RankPill rankPill = rankDao.searchPillByPillId(each.get(i).getPillId());
 
-            Rank save = mapper.mapToRank(weeks, year, i, rankPill, midCategoryId, now());
+            Rank save = mapper.mapToRank(weeks, year, i, rankPill, midCategory, now());
             infos.add(save);
         }
     }
